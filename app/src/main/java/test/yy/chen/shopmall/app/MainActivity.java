a930d75a434c167c0ba18142280929d9753df09c
chenrongfa <18720979339@163.com>
2017-01-19 09:59:20 +0800

    test

--- app/src/main/java/test/yy/chen/shopmall/app/MainActivity.java
+++ app/src/main/java/test/yy/chen/shopmall/app/MainActivity.java
@@ -1,7 +1,9 @@
-package test.yy.chen.shopmall;
+package test.yy.chen.shopmall.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import test.yy.chen.shopmall.R;
 
 public class MainActivity extends AppCompatActivity {
 
